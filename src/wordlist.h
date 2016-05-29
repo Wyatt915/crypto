caada04fbc71cd5ee63aa28ca03cf5f44e7ed980
Wyatt915 <wyatt915@gmail.com>
2016-05-29 18:38:57 -0500

    changed the OG wordlist back to the apostrophe'd version

--- src/wordlist.h
+++ src/wordlist.h
@@ -6,5 +6,6 @@ extern std::vector<std::string> wordlist;
 extern std::vector<std::string> patt;
 extern std::vector<int> freqs;
 extern int LENGTH_OF_LIST;
-void init_words();	//Use the OG wordlist
-void init_words_freq();	//Use Mark Davies' wordlist (includes frequency data)
+
+void init_words();
+void init_words_freq();
